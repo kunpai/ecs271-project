683bc516c4553ad5d851540298604cbf7728ea6b
kunpai <kunpai@ucdavis.edu>
2024-05-22 21:13:08 -0700

    GSM8K processing updates

--- dataset/gsm8k/process_gsm8k.py
+++ dataset/gsm8k/process_gsm8k.py
@@ -9,7 +9,14 @@ for line in open('data.jsonl'):
         data = json.loads(line)
         question = data['question'].strip()
         answer = data['answer'].split('####')[1].strip()
-        equation = data['answer'].split('<<')[1].split('>>')[0].strip()
+        text = data['answer']
+        start_index = text.rfind('<<') + 2
+        end_index = text.rfind('>>')
+        if start_index != -1 and end_index != -1 and start_index < end_index:
+            equation = text[start_index:end_index].strip().split('=')[0].strip()
+        else:
+            equation = ''
+        # equation = data['answer'].split('<<')[1].split('>>')[0].strip()
         with open('data.csv', 'a') as f:
             f.write(question + ';' + answer + ';' + equation + '\n')
     except:
